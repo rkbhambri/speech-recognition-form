8804391cfcefe9dc49fdcb07b70015968c62690b
ram <ramkrishan@kontikilabs.com>
2019-05-30 18:24:14 +0530

    Updates

---
+++ precache-manifest.59c2c9b4b963b146ae7cac8612aaf4a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8df95c11700519dc12715e47377bda",
+    "url": "/speech-recognition-form/index.html"
+  },
+  {
+    "revision": "274c0b4cfea84310a7b2",
+    "url": "/speech-recognition-form/static/css/2.56d65a16.chunk.css"
+  },
+  {
+    "revision": "25d6a6afb74c7ae9e441",
+    "url": "/speech-recognition-form/static/css/main.888d8b0d.chunk.css"
+  },
+  {
+    "revision": "274c0b4cfea84310a7b2",
+    "url": "/speech-recognition-form/static/js/2.f3300468.chunk.js"
+  },
+  {
+    "revision": "25d6a6afb74c7ae9e441",
+    "url": "/speech-recognition-form/static/js/main.05fe5936.chunk.js"
+  },
+  {
+    "revision": "c54191d9c96edf0cb833",
+    "url": "/speech-recognition-form/static/js/runtime~main.5ab32d15.js"
+  },
+  {
+    "revision": "364871954823eb7c0480ee99979bbe32",
+    "url": "/speech-recognition-form/static/media/voice.36487195.svg"
+  }
+]);
